37633908ca9656bc18de020c5a3d88fdedf56539
MasterSwords1 <abderrahmaneriyad15@gmail.com>
2026-02-21 20:04:06 +0000

    trying to connect wp to mariadb remotly

--- srcs/mandatory/mariadb/tools/script.sh
+++ srcs/mandatory/mariadb/tools/script.sh
@@ -8,10 +8,12 @@ chown -R mysql:root /var/run/mysqld/
 
 service mariadb start
 
-mkdir mariadb-data
+mkdir mariadb-data 2&>/dev/null
 
-if [[ $(ls -A mariadb-data/ | wc -l) == "0" ]]; then
-	mariadb-install-db --user=mysql --datadir=/mariadb-data
+if [[ $(ls -A /mariadb-data/ | wc -l) == "0" ]]; then
+	mariadb-install-db --user=mysql --datadir=/mariadb-data 2&>/dev/null
 fi
 
 mariadb -u root < db-setup.sql
+
+mariadb -u root
--- srcs/mandatory/wordpress/script.sh
+++ srcs/mandatory/wordpress/script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 if [[ $(ls -A /var/www/wordpress | wc -l) == "0" ]]; then
-	su -c "wp core download --path=/var/www/wordpress" - wordpress_user
+	su -c "wp core download --path=/var/www/wordpress --allow-root" - wordpress_user
 fi
+
+bash
