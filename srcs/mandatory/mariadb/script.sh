a018bd5fc013bae701b9236f0d9f70b6f9f80524
MasterSwords1 <abderrahmaneriyad15@gmail.com>
2026-08-06 10:53:06 +0100

    Fix wordpress startup healthchecks

--- srcs/mandatory/mariadb/script.sh
+++ srcs/mandatory/mariadb/script.sh
@@ -3,24 +3,29 @@ set -e
 
 mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
+chown -R mysql:mysql /var/lib/mysql
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
-    
-    tmp_sql="/tmp/init.sql"
-    cat << EOF > "$tmp_sql"
-FLUSH PRIVILEGES;
+
+    mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking --socket=/run/mysqld/mysqld.sock &
+
+    until mariadb --protocol=socket --socket=/run/mysqld/mysqld.sock -u root -e "SELECT 1" &>/dev/null; do
+        sleep 1
+    done
+
+    mariadb --protocol=socket --socket=/run/mysqld/mysqld.sock -u root <<EOF
 CREATE DATABASE IF NOT EXISTS wordpress_db;
 CREATE USER IF NOT EXISTS '${WP_ADMIN}'@'%' IDENTIFIED BY '${WP_ADMIN_PASS}';
+GRANT CREATE ON *.* TO '${WP_ADMIN}'@'%';
 GRANT ALL PRIVILEGES ON wordpress_db.* TO '${WP_ADMIN}'@'%';
 CREATE USER IF NOT EXISTS '${WP_USER}'@'%' IDENTIFIED BY '${WP_USER_PASS}';
 GRANT ALL PRIVILEGES ON wordpress_db.* TO '${WP_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${WP_ADMIN_PASS}';
 FLUSH PRIVILEGES;
 EOF
-    mariadbd --user=mysql --bootstrap < "$tmp_sql"
-    rm -f "$tmp_sql"
+
+    mariadb-admin --protocol=socket --socket=/run/mysqld/mysqld.sock -u root -p"${WP_ADMIN_PASS}" shutdown
 fi
 
 exec mariadbd --user=mysql
-
--- srcs/mandatory/wordpress/script.sh
+++ srcs/mandatory/wordpress/script.sh
@@ -7,17 +7,23 @@ until mariadb -h mariadb -u "${WP_ADMIN}" -p"${WP_ADMIN_PASS}" -e "SELECT 1;" &>
 done
 echo "MariaDB is online!"
 
+mariadb -h mariadb -u "${WP_ADMIN}" -p"${WP_ADMIN_PASS}" -e "CREATE DATABASE IF NOT EXISTS wordpress_db;" &>/dev/null
+
 cd /var/www/wordpress
 
-if ! wp core is-installed --allow-root; then
-    wp core download --allow-root
+if [ ! -f wp-config.php ]; then
+    if [ ! -f wp-load.php ]; then
+        wp core download --allow-root
+    fi
     wp config create \
         --allow-root \
         --dbname=wordpress_db \
         --dbuser="${WP_ADMIN}" \
         --dbhost=mariadb \
         --dbpass="${WP_ADMIN_PASS}"
-    
+fi
+
+if ! wp core is-installed --allow-root; then
     wp core install \
         --allow-root \
         --url="https://${DOMAIN_NAME:-localhost}" \
@@ -27,7 +33,8 @@ if ! wp core is-installed --allow-root; then
         --admin_email="${WP_ADMIN_EMAIL:-admin@domain.com}"
 fi
 
+groupadd -f wordpress_user
+id -u wordpress_user &>/dev/null || useradd -g wordpress_user wordpress_user
 chown -R wordpress_user:wordpress_user /var/www/wordpress
 
 exec php-fpm8.2 -F
-
